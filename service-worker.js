e20005d59d69a877d1b7da5172c6a1f07348b8b1
Jayana <jacoob.jr22@gmail.com>
2026-03-10 16:26:08 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Cache les assets statiques pour un chargement rapide
 // Ne met PAS en cache les requêtes Supabase (données toujours fraîches)
 
-var CACHE_NAME = 'yam-v12';
+var CACHE_NAME = 'yam-v13';
 
 // Assets à mettre en cache au premier chargement
 var STATIC_ASSETS = [
@@ -16,6 +16,7 @@ var STATIC_ASSETS = [
   '/yam-app/js/app-music.js',
   '/yam-app/js/app-games.js',
   '/yam-app/js/app-multiplayer.js',
+  '/yam-app/js/app-cowatch.js',
   '/yam-app/js/app-skyjo.js',
   '/yam-app/js/app-pranks.js',
   '/yam-app/js/app-messages.js',
